2194f1c9f320a2bdd353f98d13c89096d3a90ec9
shashank <jagannathamshashank@gmail.com>
2023-02-19 19:29:28 +0530

    Include Changes

--- thirdfund-app/src/pages/CampaignDetails.jsx
+++ thirdfund-app/src/pages/CampaignDetails.jsx
@@ -106,7 +106,7 @@ const CampaignDetails = () => {
               </div>
               <div>
                 <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{state.owner}</h4>
-                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Campaigns</p>
+                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Projects</p>
               </div>
             </div>
           </div>
@@ -140,13 +140,13 @@ const CampaignDetails = () => {
 
           <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
             <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
-              Fund the campaign
+              Fund the Project
             </p>
             <div className="mt-[30px]">
               <input
                 type="number"
-                placeholder="ETH 0.1"
-                step="0.01"
+                placeholder="BIT 1"
+                step="0.1"
                 className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
                 value={amount}
                 onChange={(e) => setAmount(e.target.value)}
@@ -159,7 +159,7 @@ const CampaignDetails = () => {
 
               <CustomButton
                 btnType="button"
-                title="Fund Campaign"
+                title="Fund Project"
                 styles="w-full bg-[#8c6dfd]"
                 handleClick={handleDonate}
               />
